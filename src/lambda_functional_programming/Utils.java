acd47c852a5a67bbbc07b271424f101cf4918c11
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-08-19 18:51:11 +0300

    19.08.2022

--- src/lambda_functional_programming/Utils.java
+++ src/lambda_functional_programming/Utils.java
@@ -2,15 +2,37 @@ package lambda_functional_programming;
 
 public class Utils {
 
-    public static void ayniSatirdaBosluktaYazdir(Object obj) {
+    public static void ayniSatirdaBosluklaYazdir(Object obj) {
 
-        System.out.print(obj+ " ");
-
+        System.out.print(obj + " ");
 
     }
 
 
+    public static boolean ciftElemaniSec(int x) {
 
+        return x % 2 == 0;
+
+    }
+
+
+    public static boolean tekElemaniSec(int x) {
+
+        return x % 2 != 0;
+
+    }
+
+
+    public static int karesinAl(int x) {
+
+        return x * x;
+    }
+
+
+    public static int kupunuAl(int x) {
+
+        return x * x * x;
+    }
 
 
 }
