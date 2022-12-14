d7c92524522fcb068103618dc2e9e7065fb87bf9
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-12-15 00:28:50 +0300

    15.12.2022 v1

--- src/day47_maps/C02_TekrarSayisiBulma.java
+++ src/day47_maps/C02_TekrarSayisiBulma.java
@@ -1,9 +1,6 @@
 package day47_maps;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.*;
 
 public class C02_TekrarSayisiBulma {
     public static void main(String[] args) {
