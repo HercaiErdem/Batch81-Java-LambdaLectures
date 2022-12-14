89fcbb9b5d06e30e39745b88d0be2fce84abd0c4
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-12-14 14:17:40 +0300

    v4

--- src/interviewQuestions/TersYazdirma02.java
+++ src/interviewQuestions/TersYazdirma02.java
@@ -3,6 +3,7 @@ package interviewQuestions;
 import java.util.Scanner;
 
 public class TersYazdirma02 {
+    /*
     public static void main(String[] args) {
 
 
@@ -15,18 +16,22 @@ public class TersYazdirma02 {
         }
         System.out.println(terstenNumber);
 
-
-
-
-
-
-
         }
 
+*/
+    public static void main(String[] args) {
+
+        String name = "Yarin interview var";
+        String terstenYazdirma = "";
+
+        for (int i = name.length() - 1; i >= 0; i--) {
+            terstenYazdirma = terstenYazdirma + name.charAt(i);
+        }
+        System.out.println("terstenYazdirma = " + terstenYazdirma);
 
 
     }
-
+}
 
 
 
