4ee420090754e595f93087de57a01516ca611c36
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-12-14 14:02:00 +0300

    v3

--- src/lessonReviews/Split.java
+++ src/lessonReviews/Split.java
@@ -4,14 +4,29 @@ import java.util.Arrays;
 
 public class Split {
 
-    public static void main(String[] args) {
+    /*
+        public static void main(String[] args) {
 
-        String str = "jsdkhjklahdsfuhujfeawhsajkelh";
+            String str = "jsdkhjklahdsfuhujfeawhsajkelh";
+
+            String arr[] = str.split("");
+            System.out.println(Arrays.toString(arr));
+            Arrays.sort(arr);
+            System.out.println(Arrays.toString(arr));
+
+        }
+        */
+    public static void main(String[] args) {
+        String str = "Hercai Erdem";
 
         String arr[] = str.split("");
         System.out.println(Arrays.toString(arr));
+
         Arrays.sort(arr);
         System.out.println(Arrays.toString(arr));
 
+
     }
+
+
 }
