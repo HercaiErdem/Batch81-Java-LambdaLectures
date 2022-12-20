2fbf07f026267cf9cca9a191bdeec80dc03a7041
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2022-12-20 13:33:53 +0300

    v6

--- src/lessonReviews/Split.java
+++ src/lessonReviews/Split.java
@@ -13,7 +13,6 @@ public class Split {
             System.out.println(Arrays.toString(arr));
             Arrays.sort(arr);
             System.out.println(Arrays.toString(arr));
-
         }
         */
     public static void main(String[] args) {
@@ -27,6 +26,5 @@ public class Split {
 
 
     }
-
 
 }
