4c45c035e72d7c05e6e92f89de0afe68192bccec
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-02 15:27:52 +0300

    02.01.2022 v1

--- src/lambda_functional_programming/FP05.java
+++ src/lambda_functional_programming/FP05.java
@@ -1,10 +1,8 @@
 package lambda_functional_programming;
 
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.*;
 
 public class FP05 {
     public static void main(String[] args) {
