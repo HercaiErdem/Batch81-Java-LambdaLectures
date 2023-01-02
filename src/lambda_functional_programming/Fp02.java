b1367a2573f09a7c2e406dfeebdea39ddb91ed85
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-01-02 15:30:20 +0300

    v4

--- src/lambda_functional_programming/Fp02.java
+++ src/lambda_functional_programming/Fp02.java
@@ -1,9 +1,7 @@
 package lambda_functional_programming;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
+import java.util.stream.*;
 
 public class Fp02 {
     /*
