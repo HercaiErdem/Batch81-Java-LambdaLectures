da8c11798829c5c9725157d32677f10b0a913d7a
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-06 15:59:02 +0300

    v3

--- src/interviewQuestions/KelimeyiTerstenYazdirma.java
+++ src/interviewQuestions/KelimeyiTerstenYazdirma.java
@@ -1,6 +1,6 @@
 package interviewQuestions;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class KelimeyiTerstenYazdirma {
     public static void main(String[] args) {
@@ -12,20 +12,19 @@ public class KelimeyiTerstenYazdirma {
         for (int i = isim.length() - 1; i >= 0; i--) {
             terstenIsim = terstenIsim + isim.charAt(i);
         }
-        System.out.println("Hercai Sevval Erdem in tersten yazilimi: \n" +terstenIsim);
+        System.out.println("Hercai Sevval Erdem in tersten yazilimi: \n" + terstenIsim);
 
         // 2. yol kullanicidan isim girilmesi istenirse (scanner ile)
         System.out.println("===========Scanner object ile kullanicidan isim girmesini isteriz=============");
         Scanner scan = new Scanner(System.in);
         System.out.print("Lutfen isminizi giriniz: ");
-        String isim2= scan.nextLine();
+        String isim2 = scan.nextLine();
         String tersten2 = "";
 
-        for (int i = isim2.length()-1; i>=0 ; i--) {
-            tersten2= tersten2+isim2.charAt(i);
+        for (int i = isim2.length() - 1; i >= 0; i--) {
+            tersten2 = tersten2 + isim2.charAt(i);
         }
         System.out.println(tersten2);
-
 
 
     }
