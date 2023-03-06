1a5032b1acdba80f3fbced9c09e9efda6c68e684
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-06 15:54:07 +0300

    06.03.2023 v1

--- src/interviewHazirlik/MapOlusturma01.java
+++ src/interviewHazirlik/MapOlusturma01.java
@@ -4,19 +4,19 @@ import java.util.*;
 
 public class MapOlusturma01 {
 
-        public static Map<String, Integer> createMap(){
-          Map<String,Integer> classMap = new HashMap<>();
-          classMap.put("Ayse Naz", 1234);
-          classMap.put("Ece Naz", 1235);
-          classMap.put("Nazli", 1236);
-          classMap.put("Defne", 1237);
-            return classMap;
-        }
+    public static Map<String, Integer> createMap() {
+        Map<String, Integer> classMap = new HashMap<>();
+        classMap.put("Ayse Naz", 1234);
+        classMap.put("Ece Naz", 1235);
+        classMap.put("Nazli", 1236);
+        classMap.put("Defne", 1237);
+        return classMap;
+    }
 
     public static void main(String[] args) {
         System.out.println("createMap() = " + createMap());
     }
-    }
+}
 
 
 
