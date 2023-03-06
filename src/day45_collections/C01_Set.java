78c0fac51ad91cf5df28c1d5ca26f9dff8574265
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-06 15:57:13 +0300

    v2

--- src/day45_collections/C01_Set.java
+++ src/day45_collections/C01_Set.java
@@ -1,8 +1,6 @@
 package day45_collections;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class C01_Set {
     public static void main(String[] args) {
