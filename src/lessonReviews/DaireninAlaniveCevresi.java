46f742e370a80ad6a7131f9133445a55a20d67ba
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-09 15:45:12 +0300

    v4

--- src/lessonReviews/DaireninAlaniveCevresi.java
+++ src/lessonReviews/DaireninAlaniveCevresi.java
@@ -4,30 +4,29 @@ public class DaireninAlaniveCevresi {
 
     /*
     Dairenin alanini ve cevresini hesaplayan java kodunu yaziniz.
-r=7;
-Pi=3.14
-Ipucu:
-Dairenin Cevresi : 2Pir
-Dairenin Alani : Pirr
-Ornek Cikti:
-43.96
-153.86
-     */
-    /*
-
-    public static void main(String[] args) {
+   r=7;
+   Pi=3.14
+   Ipucu:
+   Dairenin Cevresi : 2Pir
+   Dairenin Alani : Pirr
+   Ornek Cikti:
+   43.96
+   153.86
 
 
-        double r = 7;
-        double pi = 3.14;
+   public static void main(String[] args) {
 
-        double alan = (r * r) * pi; // formul yari capin karesi carpi pi
 
-        System.out.println("Dairenin alani: " + alan);
+           double r = 7;
+           double pi = 3.14;
 
-        double cevre = 2*(r*pi); // formul 2* yar cap carpi pi
-        System.out.println("Dairenin cevresi: " + cevre);
-*/
+           double alan = (r * r) * pi; // formul yari capin karesi carpi pi
+
+           System.out.println("Dairenin alani: " + alan);
+
+           double cevre = 2*(r*pi); // formul 2* yar cap carpi pi
+           System.out.println("Dairenin cevresi: " + cevre);
+   */
 
     public static void main(String[] args) {
         int r = 7;
