90712c30ec1d209f67781afdd6eab4bb613bdff7
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-12 19:47:40 +0300

    v2

--- src/day33_encapsulation/Araba.java
+++ src/day33_encapsulation/Araba.java
@@ -10,7 +10,7 @@ public class Araba {
     // private yaptigimiz model ve yakit variable'larina erisimi yetkilendirelim
     // model'e deger atanabilsin ama gorulemesin(setter)
     // yakit ise gorulebilsin ama yeni deger atanamasin isteniyor (getter)
-    //
+
     public void setModel(String model) {
         this.model = model;
     }
