d97908b36e2c044dab3cd531bb6ac99b5fa74bd0
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-15 15:06:07 +0300

    v6

--- src/day05_practice/Q02_ForLoop.java
+++ src/day05_practice/Q02_ForLoop.java
@@ -4,7 +4,8 @@ import java.util.Scanner;
 
 public class Q02_ForLoop {
 
-    /*    Kullanıcıdan 1'den büyük bir tam sayı girmesini isteyin
+       /*
+    Kullanıcıdan 1'den büyük bir tam sayı girmesini isteyin
     ve 1'den girilen tam sayıya kadar olan sayıların karelerinin toplamını
     hesaplayan kodu yazınız.
 
