172d6525cc1bdad6217a2756e45960c171730bc9
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-15 15:04:46 +0300

    v4

--- src/lambda_practice/Methodlarim.java
+++ src/lambda_practice/Methodlarim.java
@@ -9,5 +9,6 @@ public class Methodlarim {//seed (tohum) methodlar create edildi
     public static boolean ciftElemaniBul(int x) {
         return x % 2 == 0;
         // 2.yol return x % 2!=1;
+
     }
 }
