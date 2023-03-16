bee81d4b206cc30b27652fda2bc835111d417633
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-16 13:28:58 +0300

    v6

--- src/day03_scanner/C02_Swap2.java
+++ src/day03_scanner/C02_Swap2.java
@@ -10,7 +10,7 @@ public class C02_Swap2 {
 
         sayi1 = sayi1 + sayi2; // 30 20
         sayi2 = sayi1 - sayi2; // 30 10 sayi2 icin istedigimiz oldu sira sayi1'de
-        sayi1 = sayi1-sayi2 ; // 20 10
+        sayi1 = sayi1 - sayi2; // 20 10
 
         System.out.println("Sayi1'in yeni degeri: " + sayi1);
         System.out.println("Sayi2'in yeni degeri: " + sayi2);
