02d5075c7e28c493f3e0ffac6be0250fae4ef17b
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-24 16:55:15 +0300

    v4

--- src/lambda_practice/LambdaAdvanced.java
+++ src/lambda_practice/LambdaAdvanced.java
@@ -3,10 +3,10 @@ package lambda_practice;
 import java.util.*;
 
 public class LambdaAdvanced {
-    //multi arrayleri cevirirken flapMap kullaniriz yapilis sekli iki turludur
-    //1) lamda kullanarak
+    // multi arrayleri cevirirken flapMap kullaniriz yapilis sekli iki turludur
+    // 1) lamda kullanarak
     // Arrays.stream(arr).flatMap(t->Arrays.stream(t))
-    //2) method ile
+    // 2) method ile
     // Arrays.stream(arr).flatMap(Arrays::stream)
     // BU SEKLIDE ARTIK TUM STREAM METHODLARI KULLANILABILIR
     public static void main(String[] args) {
@@ -19,9 +19,9 @@ public class LambdaAdvanced {
          */
         String[][] arr = {{"Ali", "Veli", "Ayse"}, {"Hasan", "Can"}, {"Suzan"}};
         /* TASK :
-   Bir integer list oluşturunuz ve bu list’deki tum sayıların karesinin
-   toplamını bulan code create ediniz.
-    */
+     Bir integer list oluşturunuz ve bu list’deki tum sayıların karesinin
+     toplamını bulan code create ediniz.
+     */
         List<Integer> numbers = new ArrayList<>(Arrays.asList(9, 4));
         System.out.println(numbers.stream().map(t -> t * t).reduce(0, (y, g) -> y + g));
         System.out.println(numbers.stream().mapToInt(t -> t * t).sum());
