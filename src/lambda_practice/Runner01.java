33939e9f03e5f14d7e2f65f64982fc929347be36
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-24 16:53:31 +0300

    v2

--- src/lambda_practice/Runner01.java
+++ src/lambda_practice/Runner01.java
@@ -5,15 +5,15 @@ public class Runner01 extends Lambda01 {//inherite birazdan
     public static void main(String[] args) {
         // aHarfleriSilenGerisiniYazdiran(names);
         System.out.println("**************");
-        //  maxElemanBul(numbers);
+        // maxElemanBul(numbers);
         System.out.println("**************");
-        //  elemanlariTopla(numbers);
+        // elemanlariTopla(numbers);
         System.out.println("**************");
         // elemanlariCarp(numbers);
         System.out.println("**************");
-        //teklerinKareBuyuktenKucuge(numbers);
+        // teklerinKareBuyuktenKucuge(numbers);
         System.out.println("**************");
-        //sonHarfeGoreTersSira(menu);
+        // sonHarfeGoreTersSira(menu);
         System.out.println("**************");
         karkterEnFazla(menu);
         System.out.println("**************");
