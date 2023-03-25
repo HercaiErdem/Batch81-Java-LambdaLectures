1f32e3e5c868fdf1b70809a9caa2b55ef8acf366
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-25 14:11:02 +0300

    v6

--- src/day05_practice/Q04_MetCreationForLoop.java
+++ src/day05_practice/Q04_MetCreationForLoop.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class Q04_MetCreationForLoop {
 
     /*
-   Interview Question
-Write a Java program to reverse a string.
-Use for loop and create a method called
-reverseString
-Mülakat Sorusu
-Bir dizeyi tersine çevirmek için bir Java programı yazın.
-for döngüsü kullanın ve adında bir yöntem oluşturun
-tersDize
-*/
+    Interview Question
+    Write a Java program to reverse a string.
+    Use for loop and create a method called
+    reverseString
+    Mülakat Sorusu
+    Bir dizeyi tersine çevirmek için bir Java programı yazın.
+    for döngüsü kullanın ve adında bir yöntem oluşturun
+    tersDize
+    */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("bir string giriniz :");
