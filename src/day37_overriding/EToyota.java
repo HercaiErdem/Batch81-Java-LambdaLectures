44d9bd33ae616d8ff5e0408f8858be2e55bfe26b
Hercai Sevval Erdem <hercaierdem27@gmail.com>
2023-03-25 14:07:30 +0300

    v2

--- src/day37_overriding/EToyota.java
+++ src/day37_overriding/EToyota.java
@@ -1,10 +1,11 @@
 package day37_overriding;
 
-public class EToyota extends DAraba{
-    void marka(){
+public class EToyota extends DAraba {
+    void marka() {
         System.out.println("Markamiz Toyota");
     }
-    void motor(){
+
+    void motor() {
         System.out.println("Toyota araclar Toyota marka motor kullanir");
     }
 }
